681133e4f6ae9771749cf3ed6595fd44ee7c28f5
Sadullo <sadulloburiyev1992@gmail.com>
2024-01-28 20:56:54 +0500

    feat: solve zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createWriteStream, createReadStream } from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  const readFilePath = "./src/zip/files/fileToCompress.txt";
+  const writeFilePath = "./src/zip/files/archive.gz";
+
+  const gzip = createGzip();
+  const source = createReadStream(readFilePath);
+  const destination = createWriteStream(writeFilePath);
+
+  try {
+    await pipeline(source, gzip, destination);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createUnzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createWriteStream, createReadStream } from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const readFilePath = "./src/zip/files/archive.gz";
+  const writeFilePath = "./src/zip/files/fileToCompress.txt";
+
+  const unzip = createUnzip();
+  const source = createReadStream(readFilePath);
+  const destination = createWriteStream(writeFilePath);
+
+  try {
+    await pipeline(source, unzip, destination);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
 };
 
 await decompress();
