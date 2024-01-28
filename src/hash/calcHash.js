ecf0e97919112ed082e69edf210b55bd7a2966e1
Sadullo <sadulloburiyev1992@gmail.com>
2024-01-28 20:36:10 +0500

    feat: add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from "fs";
+
+const { createHash } = await import("crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readFilePath = "./src/hash/files/fileToCalculateHashFor.txt";
+
+  const hash = createHash("sha256");
+
+  const readable = createReadStream(readFilePath);
+
+  readable.on("readable", () => {
+    const data = readable.read();
+
+    if (data) hash.update(data);
+    else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
