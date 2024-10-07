2989234601733cd62a5389f4545921ddb79b9309
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-07 16:22:42 +0500

    feat: completed fs subtasks

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import { join } from "node:path";
+
+import { getAbsolutePath } from "../helpers/getAbsolutePath.js";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = getAbsolutePath(import.meta.url);
+
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+
+  try {
+    const content = await fs.readFile(filePath, { encoding: "utf8" });
+
+    console.log(content);
+  } catch (e) {
+    if (e.code === "ENOENT") throw new Error("FS operation failed");
+    else console.log(e);
+  }
 };
 
 await read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from "node:fs/promises";
+import { join } from "node:path";
+import { existsSync } from "fs";
+
+import { getAbsolutePath } from "../helpers/getAbsolutePath.js";
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = getAbsolutePath(import.meta.url);
+
+  const oldPath = join(__dirname, "files", "wrongFilename.txt");
+  const newPath = join(__dirname, "files", "properFilename.md");
+
+  try {
+    if (!existsSync(oldPath) || existsSync(newPath))
+      throw new Error("FS operation failed");
+    await fs.rename(oldPath, newPath);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await rename();
