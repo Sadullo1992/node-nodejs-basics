708bc54080594d2cddfa26ac85df61bb85556662
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-07 17:16:59 +0500

    feat: implement cli subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = argv.slice(2);
+
+  let str = "";
+  for (let i = 0; i < args.length; i += 2) {
+    str += `${args[i].slice(2)} is ${args[i + 1]}, `;
+  }
+
+  console.log(str.slice(0, -2));
 };
 
 parseArgs();
