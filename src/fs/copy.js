b91fb62002be2a842df81b7638b27243d013f2fa
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-07 15:42:00 +0500

    feat: add fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from "node:fs/promises";
+import { join } from "node:path";
+
+import { getAbsolutePath } from "../helpers/getAbsolutePath.js";
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = getAbsolutePath(import.meta.url);
+
+  const src = join(__dirname, "files");
+  const dest = join(__dirname, "files_copy");
+
+  try {
+    await fs.cp(src, dest, {
+      errorOnExist: true,
+      force: false,
+      recursive: true,
+    });
+  } catch (e) {
+    if (e.code === "ERR_FS_CP_EEXIST" || e.code === "ENOENT")
+      throw new Error("FS operation failed");
+    else console.log(e);
+  }
 };
 
 await copy();
