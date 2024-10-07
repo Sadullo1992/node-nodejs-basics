034fb1f987205d73f28e8320b3ac863021330049
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-07 21:10:42 +0500

    feat: implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { join } from "node:path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "node:stream/promises";
+import { createGzip } from "node:zlib";
+
+import { getAbsolutePath } from "../helpers/getAbsolutePath.js";
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = getAbsolutePath(import.meta.url);
+  const srcFilePath = join(__dirname, "files", "fileToCompress.txt");
+  const destFilePath = join(__dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const source = createReadStream(srcFilePath);
+  const destination = createWriteStream(destFilePath);
+
+  try {
+    await pipeline(source, gzip, destination);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { join } from "node:path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "node:stream/promises";
+import { createUnzip } from "node:zlib";
+
+import { getAbsolutePath } from "../helpers/getAbsolutePath.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = getAbsolutePath(import.meta.url);
+  const srcFilePath = join(__dirname, "files", "archive.gz");
+  const destFilePath = join(__dirname, "files", "fileToCompress.txt");
+
+  const unzip = createUnzip();
+  const source = createReadStream(srcFilePath);
+  const destination = createWriteStream(destFilePath);
+
+  try {
+    await pipeline(source, unzip, destination);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await decompress();
