14d7563da2967e93f922a8ae81b0c3456d602d81
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-07 20:44:39 +0500

    feat: implement streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { join } from "node:path";
+import { createWriteStream } from "fs";
+import { pipeline } from "node:stream/promises";
+
+import { getAbsolutePath } from "../helpers/getAbsolutePath.js";
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = getAbsolutePath(import.meta.url);
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+
+  const readable = process.stdin;
+  const writable = createWriteStream(filePath);
+
+  try {
+    await pipeline(readable, writable);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await write();
